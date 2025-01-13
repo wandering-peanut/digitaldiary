83a62457cb6abc3a1c0bffcada2f0ae3128dd14c
wandering-peanut <risaitow20@gmail.com>
2025-01-12 19:19:15 -0800

    Changed Title

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -10,10 +10,10 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	site: 'https://blog-template-gray.vercel.app/', // Write here your website url
-	author: 'Your Name', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
+	site: 'https://digitaldiary-7hgkb02ss-wandering-peanuts-projects.vercel.app/', // Write here your website url
+	author: 'peanut', // Site author
+	title: 'wandering peanut', // Site title.
+	description: 'my blog', // Description to display in the meta tags
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
 	shareMessage: 'Share this post', // Message to share a post on social media
